51dbeb38cbaa20a06329552f662b4c41159f9b71
Bavo Van Achte <bavo.van.achte@gmail.com>
2020-04-29 12:01:33 +0200

    Adding boolean and real data types

--- examples/export_test.py
+++ examples/export_test.py
@@ -22,23 +22,38 @@ content = REQ_IF_CONTENT()
 
 gen_id = ('x' + str(uuid.uuid1()))
 datatype_xhtml = DATATYPE_DEFINITION_XHTML(LONG_NAME="xhtml")
-datatype_string = DATATYPE_DEFINITION_STRING(LONG_NAME="String")
-datatype_date = DATATYPE_DEFINITION_DATE(LONG_NAME="Date")
 content.add_datatype(datatype_xhtml)
+datatype_string = DATATYPE_DEFINITION_STRING(LONG_NAME="String")
+content.add_datatype(datatype_string)
+datatype_date = DATATYPE_DEFINITION_DATE(LONG_NAME="Date")
+content.add_datatype(datatype_date)
+datatype_boolean = DATATYPE_DEFINITION_BOOLEAN(LONG_NAME="Boolean")
+content.add_datatype(datatype_boolean)
+datatype_real = DATATYPE_DEFINITION_REAL(LONG_NAME="Real", MIN=0.0, MAX=100.0, ACCURACY=2)
+content.add_datatype(datatype_real)
 
 # The specification types
 text_attribute = ATTRIBUTE_DEFINITION_XHTML(LONG_NAME="Text", datatype=datatype_xhtml)
+min_value_attribute = ATTRIBUTE_DEFINITION_REAL(LONG_NAME="Minimum Value", datatype=datatype_real)
+max_value_attribute = ATTRIBUTE_DEFINITION_REAL(LONG_NAME="Maximum Value", datatype=datatype_real)
 author_attribute = ATTRIBUTE_DEFINITION_STRING(LONG_NAME="Author", datatype=datatype_string)
 last_executed_attribute = ATTRIBUTE_DEFINITION_DATE(LONG_NAME="Last executed", datatype=datatype_date)
+approved_attribute = ATTRIBUTE_DEFINITION_BOOLEAN(LONG_NAME="Approved", datatype=datatype_boolean)
 
 requirement_object_type = SPEC_OBJECT_TYPE(LONG_NAME= "Requirement")
 requirement_object_type.add_attribute(text_attribute)
 requirement_object_type.add_attribute(author_attribute)
+requirement_object_type.add_attribute(approved_attribute)
 
 test_object_type = SPEC_OBJECT_TYPE(LONG_NAME= "Test Case")
 test_object_type.add_attribute(text_attribute)
 test_object_type.add_attribute(author_attribute)
 test_object_type.add_attribute(last_executed_attribute)
+
+configuration_object_type = SPEC_OBJECT_TYPE(LONG_NAME= "Configuration item")
+configuration_object_type.add_attribute(text_attribute)
+configuration_object_type.add_attribute(min_value_attribute)
+configuration_object_type.add_attribute(max_value_attribute)
 
 spec_relation_type = SPEC_RELATION_TYPE(LONG_NAME="selflink")
 specification_type = SPECIFICATION_TYPE(LONG_NAME="doc_type")
@@ -46,6 +61,7 @@ specification_type = SPECIFICATION_TYPE(LONG_NAME="doc_type")
 # a.CORE_CONTENT.REQ_IF_CONTENT.SPEC_TYPES = pyxb.BIND()
 content.add_spectype(requirement_object_type)
 content.add_spectype(test_object_type)
+content.add_spectype(configuration_object_type)
 content.add_spectype(spec_relation_type)
 content.add_spectype(specification_type)
 
@@ -53,15 +69,17 @@ content.add_spectype(specification_type)
 requirement_1 = SPEC_OBJECT(IDENTIFIER="SWRQT-ANGLE_CALCUL", spectype=requirement_object_type)
 requirement_1.VALUES.append(ATTRIBUTE_VALUE_XHTML(definition=text_attribute, value="The SW shall calculate an angle"))
 requirement_1.VALUES.append(ATTRIBUTE_VALUE_STRING(definition=author_attribute, value="John Doe"))
+requirement_1.VALUES.append(ATTRIBUTE_VALUE_BOOLEAN(definition=approved_attribute, value=False))
 content.add_specobject(requirement_1)
 
 requirement_2 = SPEC_OBJECT(IDENTIFIER="SWRQT-FIELD_CALCUL", spectype=requirement_object_type)
 xml_string = '<div>XY Block Adapter shall translate the Communication to TMN-Block in a bidirectional manner and support all functionalities of a TMN-Block.<br/> <object data="files/rmf-6d04fefa-8350-4870-b2bf-f4923148d064_diagram_20190628-1304.42265.mxg.png" name="diagram_20190628-1304.42265.mxg.png" type="image/png"/></div>'
 requirement_2.VALUES.append(ATTRIBUTE_VALUE_XHTML(definition=text_attribute, value=xml_string))
 requirement_2.VALUES.append(ATTRIBUTE_VALUE_STRING(definition=author_attribute, value="John Doe"))
+requirement_2.VALUES.append(ATTRIBUTE_VALUE_BOOLEAN(definition=approved_attribute, value=True))
 content.add_specobject(requirement_2)
 
-# The actual requirements
+# Test cases
 utest_1 = SPEC_OBJECT(IDENTIFIER="UTEST-ANGLE_CALCUL", spectype=test_object_type)
 utest_1.VALUES.append(ATTRIBUTE_VALUE_XHTML(definition=text_attribute, value="The SW shall test the correct functioning of the angle calculation"))
 utest_1.VALUES.append(ATTRIBUTE_VALUE_STRING(definition=author_attribute, value="Jane \n Doe"))
@@ -74,6 +92,19 @@ utest_2.VALUES.append(ATTRIBUTE_VALUE_STRING(definition=author_attribute, value=
 utest_2.VALUES.append(ATTRIBUTE_VALUE_DATE(definition=last_executed_attribute, value=datetime.today()))
 content.add_specobject(utest_2)
 
+# Configuration items
+
+config_clockspeed = SPEC_OBJECT(IDENTIFIER="CONFIG-CLOCKSPEED", spectype=configuration_object_type)
+config_clockspeed.VALUES.append(ATTRIBUTE_VALUE_XHTML(definition=text_attribute, value="A configuration parameter for configuring the clock speed shall be available"))
+config_clockspeed.VALUES.append(ATTRIBUTE_VALUE_REAL(definition=min_value_attribute, value=12.0))
+config_clockspeed.VALUES.append(ATTRIBUTE_VALUE_REAL(definition=max_value_attribute, value=36.0))
+content.add_specobject(config_clockspeed)
+
+config_pwm_accuracy = SPEC_OBJECT(IDENTIFIER="CONFIG-PWM_ACCURACY", spectype=configuration_object_type)
+config_pwm_accuracy.VALUES.append(ATTRIBUTE_VALUE_XHTML(definition=text_attribute, value="A configuration parameter for setting the PWM accuracy (number of bits)"))
+config_pwm_accuracy.VALUES.append(ATTRIBUTE_VALUE_REAL(definition=min_value_attribute, value=0.0))
+config_pwm_accuracy.VALUES.append(ATTRIBUTE_VALUE_REAL(definition=max_value_attribute, value=16.0))
+content.add_specobject(config_pwm_accuracy)
 
 spec = SPECIFICATION(IDENTIFIER="SW_specification", spectype=specification_type, LONG_NAME="SW specification")
 spec.add_spec_hierarchy(SPEC_HIERARCHY(spec_object=requirement_1))
@@ -85,6 +116,10 @@ utp.add_spec_hierarchy(SPEC_HIERARCHY(spec_object=utest_1))
 utp.add_spec_hierarchy(SPEC_HIERARCHY(spec_object=utest_2))
 content.add_specification(utp)
 
+config_params = SPECIFICATION(IDENTIFIER="CONFIGURATION_PARAMS", spectype=specification_type, LONG_NAME="Configuration parameters")
+config_params.add_spec_hierarchy(SPEC_HIERARCHY(spec_object=config_clockspeed))
+config_params.add_spec_hierarchy(SPEC_HIERARCHY(spec_object=config_pwm_accuracy))
+content.add_specification(config_params)
 
 # Relationships between requirements
 content.add_spec_relation(
--- reqif_pyxb/ReqIF.py
+++ reqif_pyxb/ReqIF.py
@@ -272,10 +272,12 @@ raw_reqif.ATTRIBUTE_DEFINITION_XHTML._SetSupersedingClass(ATTRIBUTE_DEFINITION_X
 # String values
 
 class DATATYPE_DEFINITION_STRING(raw_reqif.DATATYPE_DEFINITION_STRING):
+    ''' Default MAX_LENGTH=1000 '''
     def __init__ (self, *args, **kw):
         super().__init__(*args, **kw)
         if not self.LAST_CHANGE: self.LAST_CHANGE = dateTime.today()
         if not self.IDENTIFIER: self.IDENTIFIER = generate_unique_id()
+        if not self.MAX_LENGTH: self.MAX_LENGTH = 1000
 
 raw_reqif.DATATYPE_DEFINITION_STRING._SetSupersedingClass(DATATYPE_DEFINITION_STRING)
 
@@ -370,15 +372,112 @@ class ATTRIBUTE_DEFINITION_DATE(raw_reqif.ATTRIBUTE_DEFINITION_DATE):
 
 raw_reqif.ATTRIBUTE_DEFINITION_DATE._SetSupersedingClass(ATTRIBUTE_DEFINITION_DATE)
 
+# Boolean values
+
+class DATATYPE_DEFINITION_BOOLEAN(raw_reqif.DATATYPE_DEFINITION_BOOLEAN):
+    def __init__ (self, *args, **kw):
+        super().__init__(*args, **kw)
+        if not self.LAST_CHANGE: self.LAST_CHANGE = dateTime.today()
+        if not self.IDENTIFIER: self.IDENTIFIER = generate_unique_id()
+
+raw_reqif.DATATYPE_DEFINITION_BOOLEAN._SetSupersedingClass(DATATYPE_DEFINITION_BOOLEAN)
+
+class ATTRIBUTE_VALUE_BOOLEAN(raw_reqif.ATTRIBUTE_VALUE_BOOLEAN):
+    def __init__ (self, *args, **kw):
+        try:
+            definition = kw.pop('definition')
+            if isinstance(definition, str):
+                definition_local = definition
+            else:
+                definition_local = str(definition.IDENTIFIER)
+        except KeyError:
+            definition_local = None
+            pass
+        try:
+            value_local = kw.pop('value')
+        except KeyError:
+            value_local = None
+            pass
+        super().__init__(*args, **kw)
+        if definition_local: self.DEFINITION=definition_local
+        if value_local is not None: self.THE_VALUE=value_local
+
+raw_reqif.ATTRIBUTE_VALUE_BOOLEAN._SetSupersedingClass(ATTRIBUTE_VALUE_BOOLEAN)
+
+class ATTRIBUTE_DEFINITION_BOOLEAN(raw_reqif.ATTRIBUTE_DEFINITION_BOOLEAN):
+    def __init__ (self, *args, **kw):
+        try:
+            datatype = kw.pop('datatype')
+            if isinstance(datatype, str):
+                datatype_local = datatype
+            else:
+                datatype_local = str(datatype.IDENTIFIER)
+        except KeyError:
+            datatype_local = None
+            pass
+        super().__init__(*args, **kw)
+        if datatype_local: self.TYPE=datatype_local
+        if not self.LAST_CHANGE: self.LAST_CHANGE = dateTime.today()
+        if not self.IDENTIFIER: self.IDENTIFIER = generate_unique_id()
+
+raw_reqif.ATTRIBUTE_DEFINITION_BOOLEAN._SetSupersedingClass(ATTRIBUTE_DEFINITION_BOOLEAN)
+
+# REAL values
+
+class DATATYPE_DEFINITION_REAL(raw_reqif.DATATYPE_DEFINITION_REAL):
+    def __init__ (self, *args, **kw):
+        super().__init__(*args, **kw)
+        if not self.LAST_CHANGE: self.LAST_CHANGE = dateTime.today()
+        if not self.IDENTIFIER: self.IDENTIFIER = generate_unique_id()
+
+raw_reqif.DATATYPE_DEFINITION_REAL._SetSupersedingClass(DATATYPE_DEFINITION_REAL)
+
+class ATTRIBUTE_VALUE_REAL(raw_reqif.ATTRIBUTE_VALUE_REAL):
+    def __init__ (self, *args, **kw):
+        try:
+            definition = kw.pop('definition')
+            if isinstance(definition, str):
+                definition_local = definition
+            else:
+                definition_local = str(definition.IDENTIFIER)
+        except KeyError:
+            definition_local = None
+            pass
+        try:
+            value_local = kw.pop('value')
+        except KeyError:
+            value_local = None
+            pass
+        super().__init__(*args, **kw)
+        if definition_local: self.DEFINITION=definition_local
+        if value_local is not None: self.THE_VALUE=value_local
+
+raw_reqif.ATTRIBUTE_VALUE_REAL._SetSupersedingClass(ATTRIBUTE_VALUE_REAL)
+
+class ATTRIBUTE_DEFINITION_REAL(raw_reqif.ATTRIBUTE_DEFINITION_REAL):
+    def __init__ (self, *args, **kw):
+        try:
+            datatype = kw.pop('datatype')
+            if isinstance(datatype, str):
+                datatype_local = datatype
+            else:
+                datatype_local = str(datatype.IDENTIFIER)
+        except KeyError:
+            datatype_local = None
+            pass
+        super().__init__(*args, **kw)
+        if datatype_local: self.TYPE=datatype_local
+        if not self.LAST_CHANGE: self.LAST_CHANGE = dateTime.today()
+        if not self.IDENTIFIER: self.IDENTIFIER = generate_unique_id()
+
+raw_reqif.ATTRIBUTE_DEFINITION_REAL._SetSupersedingClass(ATTRIBUTE_DEFINITION_REAL)
+
 # Classes not overridden (yet):
 # - RELATION_GROUP
 # - RELATION_GROUP_TYPE
 # - LOCAL_REF
 # - GLOBAL_REF
 # - ALTERNATIVE_ID
-# - ATTRIBUTE_DEFINITION_BOOLEAN
-# - ATTRIBUTE_VALUE_BOOLEAN
-# - DATATYPE_DEFINITION_BOOLEAN
 
 # - ATTRIBUTE_DEFINITION_ENUMERATION
 # - DATATYPE_DEFINITION_ENUMERATION
@@ -387,10 +486,6 @@ raw_reqif.ATTRIBUTE_DEFINITION_DATE._SetSupersedingClass(ATTRIBUTE_DEFINITION_DA
 # - ATTRIBUTE_DEFINITION_INTEGER
 # - ATTRIBUTE_VALUE_INTEGER
 # - DATATYPE_DEFINITION_INTEGER
-
-# - ATTRIBUTE_DEFINITION_REAL
-# - ATTRIBUTE_VALUE_REAL
-# - DATATYPE_DEFINITION_REAL
 
 # - EMBEDDED_VALUE
 # - ENUM_VALUE
